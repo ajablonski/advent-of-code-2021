662de4bcc98c7d5d8486671a2327dac1e230c654
Alex Jablonski <2461496+ajablonski@users.noreply.github.com>
2021-12-10 17:29:17 -0600

    Shared: Add timing to main method

--- src/main/scala/com/github/ajablonski/AocProblem.scala
+++ src/main/scala/com/github/ajablonski/AocProblem.scala
@@ -6,9 +6,17 @@ import scala.util.Using
 abstract class AocProblem[T] {
   def main(args: Array[String]): Unit = {
     println("Part 1")
-    println(part1(args(0)))
+    println(profile(part1(args(0))))
     println("Part 2")
-    println(part2(args(0)))
+    println(profile(part2(args(0))))
+  }
+
+  def profile[T](block: => T): T = {
+    val t0 = System.nanoTime()
+    val result = block
+    val t1 = System.nanoTime()
+    println(s"Took ${(t1 - t0).toDouble / 1_000_000_000} seconds")
+    result
   }
 
   def part1(filename: String): T
