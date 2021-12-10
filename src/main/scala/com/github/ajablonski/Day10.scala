8a800bcd657593c19c3f3ec4c39fa9aa4dae0fb4
Alex Jablonski <2461496+ajablonski@users.noreply.github.com>
2021-12-10 17:21:42 -0600

    Day 10: Update solution to have better performance by reducing number of passes over characters

--- src/main/scala/com/github/ajablonski/Day10.scala
+++ src/main/scala/com/github/ajablonski/Day10.scala
@@ -2,7 +2,21 @@ package com.github.ajablonski
 
 import scala.annotation.tailrec
 
+
+type ChunkChar = ClosingChar | OpeningChar
+type ClosingChar = ']' | '}' | '>' | ')'
+type OpeningChar = '[' | '{' | '<' | '('
+
+object Chunk {
+  given stringToChunkChar: Conversion[String, List[ChunkChar]] =
+    _.toCharArray.toList.filter(_.isInstanceOf[ChunkChar]).map(_.asInstanceOf[ChunkChar])
+}
+
+case class ExpectedButWas(expected: ClosingChar, was: ClosingChar)
+
 object Day10 extends AocProblem[Long] {
+  import Chunk.stringToChunkChar
+
   override def part1(filename: String): Long = {
     getRawData(filename)
       .flatMap(findUnexpectedClosings)
@@ -12,39 +26,23 @@ object Day10 extends AocProblem[Long] {
 
   override def part2(filename: String): Long = {
     val scores = getRawData(filename)
-      .filter(findUnexpectedClosings(_).isEmpty)
-      .map(line => scoreCompletion(complete(line).split("").toList.asInstanceOf[List[ClosingChar]]))
+      .flatMap(line => completeOrError(line).toOption)
+      .map(scoreCompletion(_))
       .sorted
 
     scores(scores.size / 2)
   }
 
   def findUnexpectedClosings(line: String): Option[ExpectedButWas] = {
-    val bracketRegex = raw".*\[([}>)]).*".r
-    val braceRegex = raw".*\{([]>)]).*".r
-    val parenRegex = raw".*\(([]}>]).*".r
-    val angleRegex = raw".*<([]})]).*".r
-
-    removeMatchedChunks(line) match {
-      case bracketRegex(butWas) => Some(ExpectedButWas("]", butWas.asInstanceOf[ClosingChar]))
-      case braceRegex(butWas) => Some(ExpectedButWas("}", butWas.asInstanceOf[ClosingChar]))
-      case parenRegex(butWas) => Some(ExpectedButWas(")", butWas.asInstanceOf[ClosingChar]))
-      case angleRegex(butWas) => Some(ExpectedButWas(">", butWas.asInstanceOf[ClosingChar]))
-      case _ => None
-    }
-  }
-
-  def complete(line: String): String = {
-    removeMatchedChunks(line)
-      .map(completionMap)
-      .reverse
-      .mkString
+    completeOrError(line)
+      .left
+      .toOption
   }
 
   def scoreCorruption(expectedButWas: ExpectedButWas): Int = {
     corruptionScoreMap(expectedButWas.was)
   }
-  
+
   @tailrec
   def scoreCompletion(remainingSymbols: List[ClosingChar], scoreSoFar: Long = 0): Long = {
     remainingSymbols match {
@@ -54,41 +52,39 @@ object Day10 extends AocProblem[Long] {
   }
 
   @tailrec
-  def removeMatchedChunks(line: String): String = {
-    val innerChunkRegex = raw"(.*)(\{}|\[]|\(\)|<>)(.*)".r
-    val removed = line match {
-      case innerChunkRegex(leftPart, chunkPart, rightPart) => leftPart + rightPart
-      case _ => line
-    }
-    if (removed == line) {
-      line
-    } else {
-      removeMatchedChunks(removed)
+  def completeOrError(line: List[ChunkChar],
+                      expectedCharStack: List[ClosingChar] = List()): Either[ExpectedButWas, List[ClosingChar]] = {
+    line match {
+      case Nil => Right(expectedCharStack)
+      case (head: OpeningChar) :: tail => completeOrError(tail, completionCharMap(head) +: expectedCharStack)
+      case (head: ClosingChar) :: tail =>
+        if (expectedCharStack.head == head) {
+          completeOrError(tail, expectedCharStack.tail)
+        } else {
+          Left(ExpectedButWas(expectedCharStack.head, head))
+        }
     }
   }
 
   private val corruptionScoreMap: Map[ClosingChar, Int] = Map(
-    ")" -> 3,
-    "]" -> 57,
-    "}" -> 1197,
-    ">" -> 25137
+    ')' -> 3,
+    ']' -> 57,
+    '}' -> 1197,
+    '>' -> 25137
   )
 
-  private val completionMap: Map[Char, ClosingChar] = Map(
-    '(' -> ")",
-    '[' -> "]",
-    '{' -> "}",
-    '<' -> ">"
+  private val completionCharMap: Map[OpeningChar, ClosingChar] = Map(
+    '(' -> ')',
+    '[' -> ']',
+    '{' -> '}',
+    '<' -> '>'
   )
 
   private val completionScoreMap: Map[ClosingChar, Int] = Map(
-    ")" -> 1,
-    "]" -> 2,
-    "}" -> 3,
-    ">" -> 4
+    ')' -> 1,
+    ']' -> 2,
+    '}' -> 3,
+    '>' -> 4
   )
 }
 
-type ClosingChar = "]" | "}" | ">" | ")"
-
-case class ExpectedButWas(expected: ClosingChar, was: ClosingChar)
--- src/test/scala/com/github/ajablonski/Day10Test.scala
+++ src/test/scala/com/github/ajablonski/Day10Test.scala
@@ -4,6 +4,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class Day10Test extends AnyFlatSpec with Matchers with AocTestData(10) {
+  import Chunk.stringToChunkChar
 
   "part1" should "return total syntax error score" in {
     Day10.part1(inputFile) shouldBe 26397
@@ -13,47 +14,42 @@ class Day10Test extends AnyFlatSpec with Matchers with AocTestData(10) {
     Day10.part2(inputFile) shouldBe 288957
   }
 
-  "removeMatchedChunks" should "return a line with all matched chunks removed" in {
-    Day10.removeMatchedChunks("{([(<{}[<>[]}>{[]{[(<()>") shouldBe "{([(<[}>{{[("
-    Day10.removeMatchedChunks("[({(<(())[]>[[{[]{<()<>>") shouldBe "[({([[{{"
-  }
-
   "findUnexpectedClosings" should "return incorrect closing characters" in {
-    Day10.findUnexpectedClosings("{([(<{}[<>[]}>{[]{[(<()>") shouldBe Some(ExpectedButWas("]", "}"))
-    Day10.findUnexpectedClosings("[[<[([]))<([[{}[[()]]]") shouldBe Some(ExpectedButWas("]", ")"))
-    Day10.findUnexpectedClosings("[{[{({}]{}}([{[{{{}}([]") shouldBe Some(ExpectedButWas(")", "]"))
-    Day10.findUnexpectedClosings("[<(<(<(<{}))><([]([]()") shouldBe Some(ExpectedButWas(">", ")"))
-    Day10.findUnexpectedClosings("<{([([[(<>()){}]>(<<{{") shouldBe Some(ExpectedButWas("]", ">"))
+    Day10.findUnexpectedClosings("{([(<{}[<>[]}>{[]{[(<()>") shouldBe Some(ExpectedButWas(']', '}'))
+    Day10.findUnexpectedClosings("[[<[([]))<([[{}[[()]]]") shouldBe Some(ExpectedButWas(']', ')'))
+    Day10.findUnexpectedClosings("[{[{({}]{}}([{[{{{}}([]") shouldBe Some(ExpectedButWas(')', ']'))
+    Day10.findUnexpectedClosings("[<(<(<(<{}))><([]([]()") shouldBe Some(ExpectedButWas('>', ')'))
+    Day10.findUnexpectedClosings("<{([([[(<>()){}]>(<<{{") shouldBe Some(ExpectedButWas(']', '>'))
     Day10.findUnexpectedClosings("[({(<(())[]>[[{[]{<()<>>") shouldBe None
   }
 
   "scoreCorruptions" should "score corruptions based on the incorrect character" in {
-    Day10.scoreCorruption(ExpectedButWas("]", ")")) shouldBe 3
-    Day10.scoreCorruption(ExpectedButWas("}", "]")) shouldBe 57
-    Day10.scoreCorruption(ExpectedButWas("]", "}")) shouldBe 1197
-    Day10.scoreCorruption(ExpectedButWas("]", ">")) shouldBe 25137
+    Day10.scoreCorruption(ExpectedButWas(']', ')')) shouldBe 3
+    Day10.scoreCorruption(ExpectedButWas('}', ']')) shouldBe 57
+    Day10.scoreCorruption(ExpectedButWas(']', '}')) shouldBe 1197
+    Day10.scoreCorruption(ExpectedButWas(']', '>')) shouldBe 25137
   }
 
-  "complete" should "return the characters needed to complete the line" in {
-    Day10.complete("[({(<(())[]>[[{[]{<()<>>") shouldBe "}}]])})]"
-    Day10.complete("[(()[<>])]({[<{<<[]>>(") shouldBe ")}>]})"
-    Day10.complete("(((({<>}<{<{<>}{[]{[]{}") shouldBe "}}>}>))))"
-    Day10.complete("{<[[]]>}<{[{[{[]{()[[[]") shouldBe "]]}}]}]}>"
-    Day10.complete("<{([{{}}[<[[[<>{}]]]>[]]") shouldBe "])}>"
+  "completeOrError" should "return the characters needed to complete the line" in {
+    Day10.completeOrError("[({(<(())[]>[[{[]{<()<>>").map(_.mkString) shouldBe Right("}}]])})]")
+    Day10.completeOrError("[(()[<>])]({[<{<<[]>>(").map(_.mkString) shouldBe Right(")}>]})")
+    Day10.completeOrError("(((({<>}<{<{<>}{[]{[]{}").map(_.mkString) shouldBe Right("}}>}>))))")
+    Day10.completeOrError("{<[[]]>}<{[{[{[]{()[[[]").map(_.mkString) shouldBe Right("]]}}]}]}>")
+    Day10.completeOrError("<{([{{}}[<[[[<>{}]]]>[]]").map(_.mkString) shouldBe Right("])}>")
   }
 
   "scoreCompletion" should "score completion" in {
-    Day10.scoreCompletion(List(")")) shouldBe 1
-    Day10.scoreCompletion(List("]")) shouldBe 2
-    Day10.scoreCompletion(List("}")) shouldBe 3
-    Day10.scoreCompletion(List(">")) shouldBe 4
+    Day10.scoreCompletion(List(')')) shouldBe 1
+    Day10.scoreCompletion(List(']')) shouldBe 2
+    Day10.scoreCompletion(List('}')) shouldBe 3
+    Day10.scoreCompletion(List('>')) shouldBe 4
   }
 
   it should "multiply previous score by 5 before adding for multi-character completions" in {
-    Day10.scoreCompletion("}}]])})]".split("").toList.asInstanceOf[List[ClosingChar]]) shouldBe 288957
-    Day10.scoreCompletion(")}>]})".split("").toList.asInstanceOf[List[ClosingChar]]) shouldBe 5566
-    Day10.scoreCompletion("}}>}>))))".split("").toList.asInstanceOf[List[ClosingChar]]) shouldBe 1480781
-    Day10.scoreCompletion("]]}}]}]}>".split("").toList.asInstanceOf[List[ClosingChar]]) shouldBe 995444
-    Day10.scoreCompletion("])}>".split("").toList.asInstanceOf[List[ClosingChar]]) shouldBe 294
+    Day10.scoreCompletion("}}]])})]".toCharArray.toList.asInstanceOf[List[ClosingChar]]) shouldBe 288957
+    Day10.scoreCompletion(")}>]})".toCharArray.toList.asInstanceOf[List[ClosingChar]]) shouldBe 5566
+    Day10.scoreCompletion("}}>}>))))".toCharArray.toList.asInstanceOf[List[ClosingChar]]) shouldBe 1480781
+    Day10.scoreCompletion("]]}}]}]}>".toCharArray.toList.asInstanceOf[List[ClosingChar]]) shouldBe 995444
+    Day10.scoreCompletion("])}>".toCharArray.toList.asInstanceOf[List[ClosingChar]]) shouldBe 294
   }
 }
