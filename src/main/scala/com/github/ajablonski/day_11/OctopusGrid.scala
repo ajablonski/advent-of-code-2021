e582454f60a942a889aa6e1069ff9706e2b51f1e
Alex Jablonski <2461496+ajablonski@users.noreply.github.com>
2021-12-12 14:16:43 -0600

    Day 11: Minor cleanup

--- src/main/scala/com/github/ajablonski/day_11/OctopusGrid.scala
+++ src/main/scala/com/github/ajablonski/day_11/OctopusGrid.scala
@@ -7,40 +7,14 @@ case class OctopusGrid(energyLevels: Map[(Int, Int), Int]) {
   private val maxCol = energyLevels.map(_._1._2).max
 
   def step1(): (OctopusGrid, Int) = {
-    val newGrid = energyLevels
-      .map {
-        case ((row, col), energyLevel) => ((row, col), energyLevel + 1)
-      }
+    val steppedGrid = energyLevels
+      .view
+      .mapValues(_ + 1)
+      .toMap
 
-    @tailrec
-    def stepRecur(steppedEnergyLevels: Map[(Int, Int), Int], flashesSoFar: Set[(Int, Int)]): (Map[(Int, Int), Int], Int) = {
-      val flashed = steppedEnergyLevels
-        .filter(_._2 > 9)
-        .keySet ++ flashesSoFar
-
-      val coordsToUpdate = steppedEnergyLevels
-        .filter(_._2 > 9)
-        .toSeq
-        .flatMap {
-          case (coords, _) => findAdjacentCoordinates(coords)
-        }
-        .filterNot(flashed.contains)
-      val newMap = coordsToUpdate
-        .foldLeft(steppedEnergyLevels.map { case (coords, energyLevel) => (coords, if (energyLevel > 9) 0 else energyLevel) }) {
-          case (newMap, coords) => newMap.updatedWith(coords)(_.map(_ + 1))
-        }
-
-      if (newMap == steppedEnergyLevels) {
-        (newMap, flashed.size)
-      } else {
-        stepRecur(newMap, flashed)
-      }
-    }
-
-    val (finalGrid, flashes) = stepRecur(newGrid, Set())
+    val (finalGrid, flashes) = stepRecur(steppedGrid, Set())
     (OctopusGrid(finalGrid), flashes)
   }
-
 
   def allSynced(): Boolean = {
     energyLevels.forall(_._2 == 0)
@@ -48,11 +22,37 @@ case class OctopusGrid(energyLevels: Map[(Int, Int), Int]) {
 
   override def toString: String = {
     energyLevels
-      .groupMap(_._1._1) { case ((rowNum, colNum), energyLevel) => (colNum, energyLevel)}
+      .groupMap(_._1._1) { case ((rowNum, colNum), energyLevel) => (colNum, energyLevel) }
       .toSeq
       .sorted
       .map { case (_, rowValues) => rowValues.toSeq.sorted.map(_._2).mkString }
       .mkString("\n")
+  }
+
+  @tailrec
+  private def stepRecur(steppedEnergyLevels: Map[(Int, Int), Int], flashesSoFar: Set[(Int, Int)]): (Map[(Int, Int), Int], Int) = {
+    val cellsToFlash = steppedEnergyLevels
+      .filter(_._2 > 9)
+    val flashed = cellsToFlash
+      .keySet ++ flashesSoFar
+
+    val newMap = cellsToFlash
+      .toSeq
+      .flatMap {
+        case (coords, _) => findAdjacentCoordinates(coords)
+      }
+      .filterNot(flashed.contains)
+      .foldLeft(
+        steppedEnergyLevels.map { case (coords, energyLevel) => (coords, if (energyLevel > 9) 0 else energyLevel) }
+      ) {
+        case (newMap, coords) => newMap.updatedWith(coords)(_.map(_ + 1))
+      }
+
+    if (newMap == steppedEnergyLevels) {
+      (newMap, flashed.size)
+    } else {
+      stepRecur(newMap, flashed)
+    }
   }
 
   private def findAdjacentCoordinates(point: (Int, Int)): Seq[(Int, Int)] = {
